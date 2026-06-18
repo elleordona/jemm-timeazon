91aacd30f3faf6f7736e0a84c335a070e63be16f
elleordona <eto0507@hotmail.co.uk>
2026-06-18 14:27:25 +0100

    created for utility functions for controllers

--- api/controllers/products.controller.js
+++ api/controllers/products.controller.js
@@ -1,18 +1,7 @@
 // Logic for products API endpoint
-import client from 'data-api-client';
 
-//* Utility functions
-const connection = client({
-    secretArn: process.env.SECRET_ARN || "NOT_SET",
-    resourceArn: process.env.CLUSTER_ARN || "NOT_SET",
-    database: process.env.DB_NAME || "NOT_SET"
-});
-
-export const runQuery = async (sql, params = {}) => {
-    if (!sql || !sql.trim()) return;
-
-    return connection.query(sql, params);
-}
+// Imports
+import { runQuery } from './utility-functions.js'
 
 // GET products
 export const getProducts = async (req, res) => {
